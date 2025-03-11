90c721f4902894d99524d2c8ea8baf1605237adc
Andreas Alexander <andreasalexander@Andreass-MacBook-Air.local>
2025-03-12 01:24:06 +0700

    feat: add create and donate function

--- web3/contracts/Charity.sol
+++ web3/contracts/Charity.sol
@@ -14,6 +14,41 @@ contract Charity {
         uint[] donations;
     }
 
-    mapping(uint256 => Charity) public charities;
+    mapping(uint256 => Campaign) public campaigns;
     uint256 public numberOfCampaigns = 0; 
+
+    function createCampaign(address _owner, string memory _title, string memory _description, uint256 _target, uint256 _deadline, string memory _image) public returns (uint256) {
+        Campaign storage campaign = campaigns[numberOfCampaigns];
+
+        require(campaign.deadline < block.timestamp, "The deadline should be date in the future.");
+
+        campaign.owner = _owner;
+        campaign.title = _title;
+        campaign.description = _description;
+        campaign.target = _target;
+        campaign.deadline = _deadline;
+        campaign.amountCollected = 0;
+        campaign.image = _image;
+
+        numberOfCampaigns += 1;
+
+        return numberOfCampaigns - 1;
+    }
+
+    function donateToCampaign(uint256 _id) public payable {
+        uint256 amount = msg.value;
+
+        Campaign storage campaign = campaigns[_id];
+        campaign.donations.push(amount);
+
+        (bool sent, bytes memory data) = payable(campaign.owner).call{value: amount}("");
+        
+        if (sent) {
+            campaign.amountCollected = campaign.amountCollected + amount;
+        }
+    }
+
+    // function getDonators() {}
+
+    // function getAllCampaigns() {}
 }
